b05d831b47b3eab6930bc0c4e07843085c4a9a84
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-04-13 01:20:54 +0430

    Implement add and display scores

---
+++ modules/refresh.js
@@ -0,0 +1,38 @@
+const datas = document.querySelector('.datas');
+const Ranks = {
+  displayRanks: () => {
+    const getApiResponse = async () => {
+      const response = await fetch(
+        'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/gzCLQEnZzEwmtdAjWrCL/scores/',
+        {
+          method: 'GET',
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    };
+
+    getApiResponse().then((data) => {
+      datas.innerHTML = '';
+      const rankData = data.result;
+      // sort by score
+      rankData.sort((x, y) => y.score - x.score);
+      rankData.forEach((element, index) => {
+        if (index >= 0 && index < 10) {
+          datas.innerHTML += `
+             <tr>
+             <td class="data">${element.user}</td>
+             <td class="data">${element.score}</td>
+             </tr>
+           `;
+        }
+      });
+    });
+  },
+};
+
+export default Ranks;
